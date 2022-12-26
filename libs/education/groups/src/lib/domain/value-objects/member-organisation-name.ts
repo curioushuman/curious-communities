aa40a9caa8d5612c07b626db096ca1e2bb4ccb62
Mike Kelly <kelly.mike@abc.net.au>
2022-12-27 09:07:54 +1100

    chore: :memo: Adding some comments

--- libs/education/groups/src/lib/domain/value-objects/member-organisation-name.ts
+++ libs/education/groups/src/lib/domain/value-objects/member-organisation-name.ts
@@ -2,6 +2,12 @@ import { Static } from 'runtypes';
 
 import { NotEmptyString } from '@curioushuman/common';
 
+/**
+ * Member Organisation Name type specification
+ *
+ * NOTE: we're not storing this in a central location to keep coupling low
+ * it will mean duplication but it's a small price to pay for the benefits
+ */
 export const MemberOrganisationName = NotEmptyString.withBrand(
   'MemberOrganisationName'
 );

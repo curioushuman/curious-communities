191940da6de991bd75b7ead48932cdfffd6e44eb
Mike Kelly <kelly.mike@abc.net.au>
2023-01-14 09:02:16 +1100

    refactor(api-admin): Modifying the hook endpoint structure

--- apps/api/admin/lib/api-admin.stack.ts
+++ apps/api/admin/lib/api-admin.stack.ts
@@ -20,9 +20,40 @@ import {
   ParticipantsHookProps,
 } from '../src/infra/hook-participant/hook.construct';
 
+/**
+ * Stack for the admin API endpoints
+ *
+ * TODO
+ * - [ ] hook construct could very easily be moved to cdk-utils
+ */
 export class ApiAdminStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    /**
+     * Over-arching API structure for admin api
+     *
+     * This will be later, when we have our own system accessing the API
+     *
+     * /courses
+     * /courses/{courseId}
+     * /courses/{courseId}/participants
+     * /courses/{courseId}/participants/{participantId}
+     *
+     * This is for external systems to hook into
+     *
+     * WOULD HAVE PREFERRED, but not possible
+     * AWS doesn't like variables of the same name in different "ancestors"
+     * /hook/{sourceKey}/external-event/{sourceEvent}/course/{courseSourceId}?{updatedStatus?}
+     * /hook/{sourceKey}/external-event/{sourceEvent}/course/{courseSourceId}/participant/{participantSourceId}?{updatedStatus?}
+     *
+     * This is what works
+     * /hook/external-event/course/{sourceKey}/{courseSourceEvent}/{courseSourceId}?{updatedStatus?}
+     * /hook/external-event/participant/{sourceKey}/{participantSourceEvent}/{participantSourceId}?{updatedStatus?}
+     *
+     * TODO
+     * - [ ] would the preferred structure be possible if I added a method at the ancestor in question?
+     */
 
     /**
      * API Gateway
@@ -55,47 +86,81 @@ export class ApiAdminStack extends cdk.Stack {
     externalEventBusConstruct.eventBus.grantPutEventsTo(apiAdmin.role);
 
     /**
-     * Common response models
-     * i.e. these are the structures for data that will be returned from THIS API
+     * Hooks
+     * GET /hook
      */
+    const hookResource = apiAdmin.api.root.addResource('hook');
 
     /**
-     * Courses
+     * External events hook
+     * GET /hook/external-event
      */
-    const courses = apiAdmin.api.root.addResource('courses');
-    const coursesCourse = courses.addResource('{courseIdSourceValue}');
+    const externalEventResource = hookResource.addResource('external-event');
+    const externalEventHookRequestParams = {
+      'method.request.path.sourceKey': true,
+      'method.request.querystring.updatedStatus': false,
+    };
 
     /**
-     * Hook for external events
-     * GET /courses/{courseIdSourceValue}/hook/{eventType}?{updatedStatus?}
+     * Course Hooks
+     * GET /hook/external-event/course/{sourceKey}/{courseSourceEvent}/{courseSourceId}?{updatedStatus?}
      */
-    const coursesHookConstruct = new CoursesHookConstruct(
+    const courseResource = externalEventResource.addResource('course');
+    const courseSourceKeyResource = courseResource.addResource('{sourceKey}');
+    const courseSourceEventResource = courseSourceKeyResource.addResource(
+      '{courseSourceEvent}'
+    );
+    const courseSourceIdResource =
+      courseSourceEventResource.addResource('{courseSourceId}');
+    const courseExternalEventHookRequestParams = {
+      ...externalEventHookRequestParams,
+      'method.request.path.courseSourceEvent': true,
+      'method.request.path.courseSourceId': true,
+    };
+
+    /**
+     * Course hook lambda and co
+     */
+    const coursesExternalEventHookConstruct = new CoursesHookConstruct(
       this,
-      'courses-hook',
+      'cc-courses-hook-external-event-course',
       {
         apiConstruct: apiAdmin,
-        rootResource: coursesCourse.addResource('hook'),
+        rootResource: courseSourceIdResource,
+        requestParameters: courseExternalEventHookRequestParams,
         eventBus: externalEventBusConstruct.eventBus,
       } as CoursesHookProps
     );
 
     /**
-     * Participants
+     * Participant Hooks
+     * GET /hook/external-event/participant/{sourceKey}/{participantSourceEvent}/{courseSourceId}/{participantSourceId}?{updatedStatus?}
      */
-    const participants = coursesCourse.addResource('participants');
-    const participantsParticipant =
-      participants.addResource('{paxIdSourceValue}');
+    const participantResource =
+      externalEventResource.addResource('participant');
+    const participantSourceKeyResource =
+      participantResource.addResource('{sourceKey}');
+    const participantSourceEventResource =
+      participantSourceKeyResource.addResource('{participantSourceEvent}');
+    const participantSourceIdResource =
+      participantSourceEventResource.addResource('{participantSourceId}');
+    const participantExternalEventHookRequestParams = {
+      ...externalEventHookRequestParams,
+      'method.request.path.participantSourceEvent': true,
+      'method.request.path.participantSourceId': true,
+    };
 
     /**
-     * Hook for external events
-     * GET /courses/{courseIdSourceValue}/participants/{paxIdSourceValue}/hook/{eventType}?{updatedStatus?}
+     * Hook for external events on participants
+     * GET /hook/{sourceKey}/external-event/{sourceEvent}/course/{courseSourceId}/participant/{participantSourceId}?{updatedStatus?}
      */
-    const participantsHookConstruct = new ParticipantsHookConstruct(
+    const participantExternalEventHookConstruct = new ParticipantsHookConstruct(
       this,
       'participants-hook',
       {
         apiConstruct: apiAdmin,
-        rootResource: participantsParticipant.addResource('hook'),
+        rootResource: participantSourceIdResource,
+        requestParameters: participantExternalEventHookRequestParams,
         eventBus: externalEventBusConstruct.eventBus,
       } as ParticipantsHookProps
     );

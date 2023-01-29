ce1dcc8d2de9f2989d914abac1b2cd3b8c58ebcd
Mike Kelly <kelly.mike@abc.net.au>
2023-01-30 09:29:50 +1100

    test(libs): :white_check_mark: Additional tests for DynamoDbMapper

--- libs/shared/common/src/lib/adapter/implementations/dynamodb/test/dynamodb.mapper.feature
+++ libs/shared/common/src/lib/adapter/implementations/dynamodb/test/dynamodb.mapper.feature
@@ -5,12 +5,12 @@ Scenario: Successful preparation of domain source ids
   When I prepare the domain source ids
   Then I should receive a valid list
 
-# Scenario: Fail; id is not a valid idSourceValue
-#   Given I have an item with invalid source ids
-#   When I prepare the domain source ids
-#   Then I should receive a ValidationError
+Scenario: Fail; id is not a valid idSourceValue
+  Given I have an item with invalid source ids
+  When I prepare the domain source ids
+  Then I should receive a SourceInvalidError
 
-# Scenario: Successful preparation of persistence source ids
-#   Given I have an entity with valid source ids
-#   When I prepare the persistence source ids
-#   Then I should receive a valid list
+Scenario: Successful preparation of persistence source fields
+  Given I have an entity with valid source fields
+  When I prepare the persistence source fields
+  Then I should receive a valid list
--- libs/shared/common/src/lib/adapter/implementations/dynamodb/test/dynamodb.mapper.spec.ts
+++ libs/shared/common/src/lib/adapter/implementations/dynamodb/test/dynamodb.mapper.spec.ts
@@ -1,3 +1,4 @@
+import { SourceInvalidError } from '@curioushuman/error-factory';
 import { loadFeature, defineFeature } from 'jest-cucumber';
 
 import { DynamoDbMapper } from '../dynamodb.mapper';
@@ -23,20 +24,45 @@ const feature = loadFeature('./dynamodb.mapper.feature', {
 });
 
 defineFeature(feature, (test) => {
-  let item: TestItem;
-  let sourceIds: TestSourceIdSource[];
+  let itemValid: TestItem;
+  let persistenceSourceFieldsValid: Record<string, string | undefined>;
+  let sourceIdsValid: TestSourceIdSource[];
+  const sources = ['ALPHA', 'BETA'];
+
+  beforeAll(() => {
+    persistenceSourceFieldsValid = {
+      Test_SourceIdALPHA: 'ALPHA#a123',
+      Test_SourceIdBETA: 'BETA#b234',
+    };
+    itemValid = {
+      primaryKey: 'test1',
+      sortKey: 'test2',
+      Test_SourceIdALPHA: 'ALPHA#a123',
+      Test_SourceIdBETA: 'BETA#b234',
+    };
+    sourceIdsValid = [
+      {
+        id: 'a123',
+        source: 'ALPHA',
+      },
+      {
+        id: 'b234',
+        source: 'BETA',
+      },
+    ];
+  });
 
   test('Successful preparation of domain source ids', ({
     given,
     when,
     then,
   }) => {
+    let item: TestItem;
+    let sourceIds: TestSourceIdSource[];
+
     given('I have an item with valid source ids', () => {
       item = {
-        primaryKey: 'test1',
-        sortKey: 'test2',
-        Test_SourceIdALPHA: 'ALPHA#a123',
-        Test_SourceIdBETA: 'BETA#b234',
+        ...itemValid,
       };
     });
 
@@ -44,7 +70,7 @@ defineFeature(feature, (test) => {
       sourceIds = DynamoDbMapper.prepareDomainSourceIds<
         TestItem,
         TestSourceIdSource
-      >(item, 'Test', ['ALPHA', 'BETA']);
+      >(item, 'Test', sources);
     });
 
     then('I should receive a valid list', () => {
@@ -58,6 +84,60 @@ defineFeature(feature, (test) => {
           source: 'BETA',
         }),
       ]);
+    });
+  });
+
+  test('Fail; id is not a valid idSourceValue', ({ given, when, then }) => {
+    let item: TestItem;
+    let sourceIds: TestSourceIdSource[];
+    let error: Error;
+
+    given('I have an item with invalid source ids', () => {
+      item = {
+        ...itemValid,
+        Test_SourceIdALPHA: 'a123',
+      };
+    });
+
+    when('I prepare the domain source ids', async () => {
+      try {
+        sourceIds = DynamoDbMapper.prepareDomainSourceIds<
+          TestItem,
+          TestSourceIdSource
+        >(item, 'Test', sources);
+      } catch (err) {
+        error = err;
+      }
+    });
+
+    then('I should receive a SourceInvalidError', () => {
+      expect(error).toBeInstanceOf(SourceInvalidError);
+      expect(sourceIds).toBeUndefined();
+    });
+  });
+
+  test('Successful preparation of persistence source fields', ({
+    given,
+    when,
+    then,
+  }) => {
+    let sourceFields: Record<string, string | undefined>;
+
+    given('I have an entity with valid source fields', () => {
+      // above
+    });
+
+    when('I prepare the persistence source fields', async () => {
+      sourceFields =
+        DynamoDbMapper.preparePersistenceSourceIdFields<TestSourceIdSource>(
+          sourceIdsValid,
+          'Test',
+          sources
+        );
+    });
+
+    then('I should receive a valid list', () => {
+      expect(sourceFields).toEqual(persistenceSourceFieldsValid);
     });
   });
 });

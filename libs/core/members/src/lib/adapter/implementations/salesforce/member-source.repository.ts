8d14e28ddfeb81e5065cadfb9f28f122552d0910
Mike Kelly <kelly.mike@abc.net.au>
2023-01-31 16:37:45 +1100

    refactor(members): Updating members to use improved SF repo

--- libs/core/members/src/lib/adapter/implementations/salesforce/member-source.repository.ts
+++ libs/core/members/src/lib/adapter/implementations/salesforce/member-source.repository.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
 import * as TE from 'fp-ts/lib/TaskEither';
 
 import { LoggableLogger } from '@curioushuman/loggable';
 import {
   confirmSourceId,
   SalesforceApiQueryField,
-  SalesforceApiRepositoryError,
-  SalesforceApiSourceRepository,
+  SalesforceApiRepository,
   SourceRepository,
+  SalesforceApiRepositoryProps,
 } from '@curioushuman/common';
 
 import {
@@ -21,10 +20,7 @@ import {
   MemberSourceFindMethod,
   MemberSourceRepository,
 } from '../../ports/member-source.repository';
-import {
-  SalesforceApiMemberSourceResponse,
-  SalesforceApiMemberSourceResponses,
-} from './entities/member-source.response';
+import { SalesforceApiMemberSourceResponse } from './entities/member-source.response';
 import { SalesforceApiMemberSourceMapper } from './member-source.mapper';
 import { MemberEmail } from '../../../domain/value-objects/member-email';
 import { RepositoryItemNotFoundError } from '@curioushuman/error-factory';
@@ -33,100 +29,95 @@ import { MemberSourceIdSource } from '../../../domain/value-objects/member-sourc
 
 @Injectable()
 export class SalesforceApiMemberSourceRepository
-  extends SalesforceApiSourceRepository
   implements MemberSourceRepository, SourceRepository<Source>
 {
+  private salesforceApiRepository: SalesforceApiRepository<
+    MemberSource,
+    SalesforceApiMemberSourceResponse
+  >;
+
   /**
    * The key for this source
    */
   public readonly SOURCE = 'CRM';
 
   constructor(public httpService: HttpService, public logger: LoggableLogger) {
-    super('Contact', SalesforceApiMemberSourceResponse);
     this.logger.setContext(SalesforceApiMemberSourceRepository.name);
+
+    // set up the repository
+    const props: SalesforceApiRepositoryProps = {
+      sourceName: 'Contact',
+      responseRuntype: SalesforceApiMemberSourceResponse,
+    };
+    this.salesforceApiRepository = new SalesforceApiRepository(
+      props,
+      this.httpService,
+      this.logger
+    );
   }
 
-  processFindOne(
-    item?: SalesforceApiMemberSourceResponse,
-    uri = 'not provided'
-  ): MemberSource {
-    // did we find anything?
-    if (!item) {
-      throw new RepositoryItemNotFoundError(`Member not found for uri: ${uri}`);
-    }
+  processFindOne =
+    (source: Source) =>
+    (
+      item?: SalesforceApiMemberSourceResponse,
+      uri = 'not provided'
+    ): MemberSource => {
+      // did we find anything?
+      if (!item) {
+        throw new RepositoryItemNotFoundError(
+          `Member not found for uri: ${uri}`
+        );
+      }
 
-    // is it what we expected?
-    // will throw error if not
-    const memberItem = SalesforceApiMemberSourceResponse.check(item);
+      // is it what we expected?
+      // will throw error if not
+      const memberItem = SalesforceApiMemberSourceResponse.check(item);
 
-    // NOTE: if the response was invalid, an error would have been thrown
-    // could this similarly be in a serialisation decorator?
-    return SalesforceApiMemberSourceMapper.toDomain(memberItem, this.SOURCE);
-  }
+      // NOTE: if the response was invalid, an error would have been thrown
+      // could this similarly be in a serialisation decorator?
+      return SalesforceApiMemberSourceMapper.toDomain(memberItem, source);
+    };
 
+  /**
+   * ? should the confirmSourceId also be in a tryCatch or similar?
+   */
   findOneByIdSource = (
     value: MemberSourceIdSource
   ): TE.TaskEither<Error, MemberSource> => {
-    return TE.tryCatch(
-      async () => {
-        // NOTE: this will throw an error if the value is invalid
-        const id = confirmSourceId<MemberSourceIdSource>(
-          MemberSourceIdSource.check(value),
-          this.SOURCE
-        );
-        const uri = this.prepareFindOneUri(id);
-        const fields = this.fields();
-        const request$ =
-          this.httpService.get<SalesforceApiMemberSourceResponse>(uri, {
-            params: {
-              fields,
-            },
-          });
-        const response = await firstValueFrom(request$);
-
-        // NOTE: if not found, an error would have been thrown and caught
-
-        return this.processFindOne(response.data, uri);
-      },
-      // NOTE: we don't use an error factory here, it is one level up
-      (reason: SalesforceApiRepositoryError) => reason as Error
+    // NOTE: this will throw an error if the value is invalid
+    const id = confirmSourceId<MemberSourceIdSource>(
+      MemberSourceIdSource.check(value),
+      this.SOURCE
+    );
+    return this.salesforceApiRepository.tryFindOne(
+      id,
+      this.processFindOne(this.SOURCE)
     );
   };
 
   findOneByEmail = (value: MemberEmail): TE.TaskEither<Error, MemberSource> => {
-    return TE.tryCatch(
-      async () => {
-        const email = MemberEmail.check(value);
-        const values: SalesforceApiQueryField[] = [
-          {
-            field: 'Email',
-            value: email,
-          },
-          {
-            field: 'npe01__HomeEmail__c',
-            value: email,
-          },
-          {
-            field: 'npe01__WorkEmail__c',
-            value: email,
-          },
-          {
-            field: 'npe01__AlternateEmail__c',
-            value: email,
-          },
-        ];
-        const uri = this.prepareQueryUri(values, 'OR');
-        // NOTE: this query will return an array of results
-        const request$ =
-          this.httpService.get<SalesforceApiMemberSourceResponses>(uri);
-        const response = await firstValueFrom(request$);
-
-        // NOTE: if not found, an error would have been thrown and caught
-
-        return this.processFindOne(response.data.records?.[0], uri);
+    const email = MemberEmail.check(value);
+    const values: SalesforceApiQueryField[] = [
+      {
+        field: 'Email',
+        value: email,
       },
-      // NOTE: we don't use an error factory here, it is one level up
-      (reason: SalesforceApiRepositoryError) => reason as Error
+      {
+        field: 'npe01__HomeEmail__c',
+        value: email,
+      },
+      {
+        field: 'npe01__WorkEmail__c',
+        value: email,
+      },
+      {
+        field: 'npe01__AlternateEmail__c',
+        value: email,
+      },
+    ];
+    return this.salesforceApiRepository.tryQueryOne(
+      values,
+      this.processFindOne(this.SOURCE)
     );
   };
 

78b3c37196b303374ff5365884171a2b5801ba5d
Mike Kelly <kelly.mike@abc.net.au>
2023-03-11 07:07:49 +1100

    docs(courses): :memo: Diagrams for course created and updated

---
+++ apps/education/courses/diagrams/course-updated.py
@@ -0,0 +1,37 @@
+from diagrams import Diagram
+from diagrams.aws.compute import LambdaFunction
+from diagrams.aws.integration import EventbridgeCustomEventBusResource, SimpleQueueServiceSqs
+
+graph_attr = {
+    "overlap": "false"
+}
+
+with Diagram("Course updated", show=False, filename="course-updated", direction="TB", graph_attr=graph_attr):
+  ext_event_bus = EventbridgeCustomEventBusResource("EXT\ncourse\ncreated")
+  int_event_bus_course_created = EventbridgeCustomEventBusResource("INT\ncourse\ncreated")
+  int_event_bus_course_updated = EventbridgeCustomEventBusResource("INT\ncourse\nupdated")
+  int_event_bus_participant_created = EventbridgeCustomEventBusResource("INT\nparticipant\ncreated")
+  int_event_bus_participant_updated = EventbridgeCustomEventBusResource("INT\nparticipant\nupdated")
+  sqs_participant_update = SimpleQueueServiceSqs("participant-update")
+  sqs_participant_upsert = SimpleQueueServiceSqs("participant-upsert")
+  lambda_course_upsert = LambdaFunction("course-upsert")
+  lambda_participant_update = LambdaFunction("participant-update")
+  lambda_participant_upsert = LambdaFunction("participant-upsert")
+  lambda_participant_update_multi = LambdaFunction("participant-update-multi")
+  lambda_participant_upsert_multi = LambdaFunction("participant-upsert-multi")
+
+  ext_event_bus >> lambda_course_upsert
+  lambda_course_upsert >> int_event_bus_course_created
+  lambda_course_upsert >> int_event_bus_course_updated
+
+  int_event_bus_course_created >> lambda_participant_upsert_multi
+  lambda_participant_upsert_multi >> sqs_participant_upsert
+  sqs_participant_upsert >> lambda_participant_upsert
+  lambda_participant_upsert >> int_event_bus_participant_created
+  lambda_participant_upsert >> int_event_bus_participant_updated
+
+  int_event_bus_course_updated >> lambda_participant_update_multi
+  lambda_participant_update_multi >> sqs_participant_update
+  sqs_participant_update >> lambda_participant_update
+  lambda_participant_update >> int_event_bus_participant_updated
+
--- apps/education/courses/diagrams/course-upsert.py
+++
@@ -1,6 +0,0 @@
-from diagrams import Diagram
-from diagrams.aws.compute import LambdaFunction
-from diagrams.aws.integration import EventbridgeCustomEventBusResource
-
-with Diagram("Course created", show=False, filename="course-upsert"):
-  EventbridgeCustomEventBusResource("EXT-course-created") >> LambdaFunction("course-upsert")

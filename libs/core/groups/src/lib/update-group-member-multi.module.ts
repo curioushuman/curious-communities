9625399730bef79a9191746541f8deb20b4a75b7
Mike Kelly <kelly.mike@abc.net.au>
2023-03-20 06:31:35 +1100

    fix(groups): Update group member multi

--- libs/core/groups/src/lib/update-group-member-multi.module.ts
+++ libs/core/groups/src/lib/update-group-member-multi.module.ts
@@ -3,19 +3,19 @@ import { CqrsModule } from '@nestjs/cqrs';
 
 import { LoggableLogger, LoggableModule } from '@curioushuman/loggable';
 import { DynamoDbRepositoryErrorFactory } from '@curioushuman/common';
-import { FindGroupMemberHandler } from './application/queries/find-group-member/find-group-member.query';
 import { GroupMemberRepository } from './adapter/ports/group-member.repository';
 import { DynamoDbGroupMemberRepository } from './adapter/implementations/dynamodb/group-member.repository';
 import { GroupMemberRepositoryErrorFactory } from './adapter/ports/group-member.repository.error-factory';
 import { UpdateGroupMemberMultiController } from './infra/update-group-member-multi/update-group-member-multi.controller';
 import { GroupsQueueService } from './adapter/ports/groups.queue-service';
 import { SqsGroupsQueueService } from './adapter/implementations/sqs/groups.queue-service';
+import { FindGroupMembersHandler } from './application/queries/find-group-members/find-group-members.query';
 
 const imports = [CqrsModule, LoggableModule];
 
 const controllers = [UpdateGroupMemberMultiController];
 
-const handlers = [FindGroupMemberHandler];
+const handlers = [FindGroupMembersHandler];
 
 const repositories = [
   {

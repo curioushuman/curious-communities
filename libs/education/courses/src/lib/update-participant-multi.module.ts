8ee1972960424eb9b40bd8efe0eae93314b329fd
Mike Kelly <kelly.mike@abc.net.au>
2023-03-20 06:05:31 +1100

    fix(courses): Update participant multi

--- libs/education/courses/src/lib/update-participant-multi.module.ts
+++ libs/education/courses/src/lib/update-participant-multi.module.ts
@@ -3,19 +3,19 @@ import { CqrsModule } from '@nestjs/cqrs';
 
 import { LoggableLogger, LoggableModule } from '@curioushuman/loggable';
 import { DynamoDbRepositoryErrorFactory } from '@curioushuman/common';
-import { FindParticipantHandler } from './application/queries/find-participant/find-participant.query';
 import { ParticipantRepository } from './adapter/ports/participant.repository';
 import { DynamoDbParticipantRepository } from './adapter/implementations/dynamodb/participant.repository';
 import { ParticipantRepositoryErrorFactory } from './adapter/ports/participant.repository.error-factory';
 import { UpdateParticipantMultiController } from './infra/update-participant-multi/update-participant-multi.controller';
 import { CoursesQueueService } from './adapter/ports/courses.queue-service';
 import { SqsCoursesQueueService } from './adapter/implementations/sqs/courses.queue-service';
+import { FindParticipantsHandler } from './application/queries/find-participants/find-participants.query';
 
 const imports = [CqrsModule, LoggableModule];
 
 const controllers = [UpdateParticipantMultiController];
 
-const handlers = [FindParticipantHandler];
+const handlers = [FindParticipantsHandler];
 
 const repositories = [
   {
